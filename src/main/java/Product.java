21c3bd853d6d58f906d48e4700ef2216ee6c940f
WallyLDZ <waliszeksebatian@gmail.com>
2023-09-19 19:29:28 +0200

    Create class Product.java

---
+++ src/main/java/Product.java
@@ -0,0 +1,59 @@
+public class Product {
+    private int Id;
+    private String Name;
+    private String category;
+    private double price;
+    private String description;
+    private String specification;
+   // zdjęcia
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+
+}
