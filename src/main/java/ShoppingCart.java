c87734b6c97bb7aecd3ba0779ca46fd0e4819b23
Sandriel93 <michalwasiak93@wp.pl>
2023-09-20 20:09:49 +0200

    kan-12 working on class

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,2 +1,79 @@
+
+/*
+pola klasy ShoppingCart:
+
+cartID : unikalny numer koszyka generowany w momencie dodania produktu do koszyka (long)
+
+currentUser: login zalogowanego użytkownika , pusty w przypadku użytkownika niezalogowanego(String) +
+
+shoppingList : lista produktów (ArrayList) +
+
+cartStatus : status koszyka(String lub enum)
+
+totalPrice : suma całkowita (double)
+
+totalPriceAfterDiscount : cena po zniżkach(double)
+
+discount: zniżka/voucher promocyjny(double)
+
+metody:
+
+addToCart - dodanie produktu do koszyka (zwraca boolean - true jak został dodany do koszyka)
+
+removeFromCart : usunięcie produktu z koszyka ( zwraca Product - produkt usunięty z koszyka)
+
+addDiscount : dodanie zniżki (void)
+
+deleteCart : usunięcie koszyka(void)
+
+
+ */
+
+import java.util.ArrayList;
+
 public class ShoppingCart {
+    private long cartID;
+    private String currentUser;
+    ArrayList<Product> shoppingList = new ArrayList<>();
+    private String cartStatus;
+    private double discount;
+    private double totalPriceAfterDiscount = 0;
+    private double totalPrice = 0;
+
+
+    public void setCurrentUser(String currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public String getCurrentUser() {
+        return currentUser;
+    }
+
+
+    public double getTotalPrice() {
+
+        return totalPrice;
+    }
+
+    public void addDiscount(double discount) {
+        this.discount = discount;
+    }
+
+
+    public void addToCart(Product product) {
+        shoppingList.add(product);
+        totalPrice+=product.getPrice();
+    }
+
+    public void removeFromCart(Product product){
+        shoppingList.remove(product);
+        totalPrice-=product.getPrice();
+    }
+
+    private long generateCartID() {
+        // Generowanie losowej 10-cyfrowej liczby
+        return 1000000000L + (long) (Math.random() * (9999999999L - 1000000000L + 1));
+    }
+
 }
+
