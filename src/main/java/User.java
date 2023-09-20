0708c685ed3d5a86dd7250ac3f222db71212c970
Shan <shantais90@gmail.com>
2023-09-20 19:39:33 +0200

    Developing User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class User {
+    private Boolean loggedStatus = false;
+    public static Boolean registrationForm() {
+
+        return true;
+    }
+
+    public static boolean isLogged() {
+        return loggedStatus;
+    }
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,7 +1,28 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    @Test
+    void isCurrentUserLogged(){
+        assertEquals(false, User.isLogged());
+    }
+    @Test
+    void areAllRegistrationSpacesFilled(){
+        String name = "Jan";
+        String surname = "Kowalski";
+        String password = "AwesomePassword123";
+        String email = "jan.kowalski@somesite.com";
+        String street = "Awesome Street";
+        String houseNumber = "111";
+        String flatNumber = "1";
+        String postalCode = "11-111";
+        String city = "Great City";
+        String phoneNumber = "123456789";
+
+        assertEquals(true, User.registrationForm());
+    }
 }
