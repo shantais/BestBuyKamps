f4dc6fafb46ae79095e6c95c2d0ec63564f7de2c
Shan <shantais90@gmail.com>
2023-09-21 11:11:44 +0200

    Added SaveToDatabase feature

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,45 +1,71 @@
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 public class User {
     private Boolean loggedStatus = false;
-    String login;
-    private String password;
-    private String name;
-    private String surname;
-    private String email;
-    private String street;
-    private String houseNumber;
-    private String flatNumber;
-    private String postalCode;
-    private String city;
-    private String phoneNumber;
-
+    String username;
+    private String password = "";
+    private String name = "";
+    private String surname = "";
+    private String email = "";
+    private String street = "";
+    private String houseNumber = "";
+    private String flatNumber = "";
+    private String postalCode = "";
+    private String city = "";
+    private String phoneNumber = "";
     public boolean isLogged() {
         return loggedStatus;
     }
 
-    public boolean registrationForm() {
+    public String registrationForm(){
+        List<String> exampleFormStrings = List.of("KowalskiRządzi666", "AwesomePassword123", "Jan", "Kowalski", "jan.kowalski@somesite.com", "Awesome Street", "111\\1", "11-111", "Great City", "123456789");
+        List<String> formStrings = List.of("username=", "password=", "name=", "surname=", "email=", "street=", "houseNumber=", "postalCode=", "city=", "phoneNumber=");
+        List<String> registrationForm = new LinkedList<>();
+
+        System.out.println("Please fill in registration form:");
+        for (String formSpace : formStrings){
+            String value = exampleFormStrings.get(formStrings.indexOf(formSpace));
+            System.out.print("\n" + formSpace + " " + value);
+            registrationForm.add(formSpace+value);
+        }
+        System.out.println('\n');
+        System.out.println(registrationForm);
+
         Path path = Paths.get("src", "main", "java", "dataFiles", "UserData.txt");
+        String formReadyToSave = String.join("|", registrationForm);
 
+        List<String> fullUserData = readFromFile(path);
 
-        return true;
+        if (!isInFile(fullUserData, username, email)){
+            writeToFile(path, fullUserData, formReadyToSave);
+        }
+        return formReadyToSave;
     }
 
-    private void saveRegistrationForm(){
-        login = "KowalskiRządzi666";
-        password = "AwesomePassword123";
-        name = "Jan";
-        surname = "Kowalski";
-        email = "jan.kowalski@somesite.com";
-        street = "Awesome Street";
-        houseNumber = "111";
-        flatNumber = "1";
-        postalCode = "11-111";
-        city = "Great City";
-        phoneNumber = "123456789";
-
-
+    private boolean isInFile(List<String> fullUserData, String username, String email){
+        return fullUserData.contains(username) || fullUserData.contains(email);
+    }
+    public List<String> readFromFile(Path path){
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+            List<String> fullUserData = new LinkedList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fullUserData.add(line);
+            }
+            return fullUserData;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void writeToFile(Path path, List<String> fullUserData, String formReadyToSave){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))){
+            fullUserData.add(formReadyToSave);
+            writer.write(String.join("\n", fullUserData));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,33 +1,57 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+    User user = new User();
 
     @Test
     void isCurrentUserLogged(){
-        User user = new User();
         assertEquals(false, user.isLogged());
     }
 
-    @Test
-    void xxx(){
+//    Tested Method but changed to private for safety
+//    TODO: try to activate via reflection
+//    @Test
+//    @Disabled
+//    void isUserNameInUserDataFile(){
+//        Path path = Paths.get("src", "main", "java", "dataFiles", "UserData.txt");
+//        assertEquals(true, user.isInFile(path, "KowalskiRządzi666", "jan.kowalski@somesite.com"));
+//    }
 
+//    test for another Class
+//    @Test
+//    void isLoginUnique(){
+//        String login = "KowalskiRządzi666";
+//        assertEquals(true, loggedInUser.getLogin(login));
+//    }
+
+    @Test
+    void didSaveRegistrationForm(){
+        //given//when
+        Path path = Paths.get("src", "main", "java", "dataFiles", "UserData.txt");
+        String userData = "username=KowalskiRządzi666|password=AwesomePassword123|name=Jan|surname=Kowalski|email=jan.kowalski@somesite.com|" +
+                "street=Awesome Street|houseNumber=111\\1|postalCode=11-111|city=Great City|phoneNumber=123456789";
+        //then
+        assertEquals(userData, user.registrationForm());
     }
 
     @Test
-    void isLoginUnique(){
-        String login = "KowalskiRządzi666";
-//        assertEquals(false, loggedInUser.getLogin(login));
+    void isRegistrationFormInDatabase(){
+        Path path = Paths.get("src", "main", "java", "dataFiles", "UserData.txt");
+        String userData = "username=KowalskiRządzi666|password=AwesomePassword123|name=Jan|surname=Kowalski|email=jan.kowalski@somesite.com|" +
+                "street=Awesome Street|houseNumber=111\\1|postalCode=11-111|city=Great City|phoneNumber=123456789";
+        assertEquals(true, user.readFromFile(path).contains(userData));
     }
 
     @Test
     void areAllRequiredRegistrationSpacesFilled(){
-        User user = new User();
-
-        String login = "KowalskiRządzi666";
+        String username = "KowalskiRządzi666";
         String password = "AwesomePassword123";
         String email = "jan.kowalski@somesite.com";
 
