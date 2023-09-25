5d53a15f87085424185fbf5058695f31206f9dcf
Krzysztof.L <k.lechniuk@gmail.com>
2023-09-25 19:56:00 +0200

    Add class Test

--- src/test/java/NewsletterTest.java
+++ src/test/java/NewsletterTest.java
@@ -1,2 +1,52 @@
-class NewsletterTest {
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.*;
+import java.nio.file.*;
+
+public class NewsletterTest {
+    private static final String TEST_FILE = "test_subscribers.txt";
+
+    @BeforeEach
+    public void setUp() {
+        createTestFile();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        deleteTestFile();
+    }
+
+    @Test
+    public void testEmailExistWhenEmailExists() {
+        assertTrue(Newsletter.emailExist("new@example.com"));
+    }
+
+    @Test
+    public void testEmailExistWhenEmailDoesNotExist() {
+        assertFalse(Newsletter.emailExist("nonnew@example.com"));
+    }
+
+    @Test
+    public void testAddEmail() {
+        String newEmail = "new@example.com";
+        Newsletter.addEmail(newEmail);
+        assertTrue(Newsletter.emailExist(newEmail));
+    }
+
+    private void createTestFile() {
+        try {
+            Files.write(Paths.get(TEST_FILE), "new@example.com\n".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteTestFile() {
+        try {
+            Files.deleteIfExists(Paths.get(TEST_FILE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
