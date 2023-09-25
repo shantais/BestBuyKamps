a83cdd7f62678c3115f5c49040265049a955f3c1
Sandriel93 <michalwasiak93@wp.pl>
2023-09-25 17:42:38 +0200

    kan-12 finishing touches

---
+++ src/test/java/ShoppingCartTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import user.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+    private ShoppingCart shoppingCart;
+    private Product product1;
+    private Product product2;
+
+    @BeforeEach
+    public void setUp() {
+        // Inicjalizacja obiektu ShoppingCart i produktów przed każdym testem
+        shoppingCart = new ShoppingCart();
+        product1 = new Product("telefon", "elektronika", 2000.00, "najlepszy telefon", "ekran i reszta bajerów");
+    }
+
+    @Test
+    public void testAddToCart() {
+        assertTrue(shoppingCart.addToCart(product1));
+        assertEquals(1, shoppingCart.getProducts().size());
+        assertEquals(2000.00, shoppingCart.getTotalPrice());
+    }
+
+    @Test
+    public void testAddDiscount() {
+        shoppingCart.addDiscount(10.0);
+        assertEquals(10.0, shoppingCart.getDiscount());
+    }
+
+    @Test
+    public void testRemoveFromCart() {
+        shoppingCart.addToCart(product1);
+        shoppingCart.removeFromCart(product1);
+        assertFalse(shoppingCart.getProducts().contains(product1));
+        assertEquals(0, shoppingCart.getProducts().size());
+    }
+
+    @Test
+    public void testGenerateCartID() {
+        long cartID = shoppingCart.getCartID();
+        assertTrue(cartID >= 1000000000L && cartID <= 9999999999L);
+    }
+
+
+}
