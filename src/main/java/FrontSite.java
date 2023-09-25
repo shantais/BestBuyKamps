c74d72ca0b66595e8c56a7cbe33594472c3418e4
Sandriel93 <michalwasiak93@wp.pl>
2023-09-25 17:50:18 +0200

    kan-12 finishing touches, fix imports

--- src/main/java/FrontSite.java
+++ src/main/java/FrontSite.java
@@ -6,5 +6,19 @@ public class FrontSite {
         System.out.println("Witamy w sklepie BestBuy Kamps");
         System.out.println("dostępne opcje dla użytkownika niezalogowanego");
         System.out.println("1. Logowanie | 2. Przejdź do produktów | 3. Przejdź do koszyka |  4.Newsletter");
+
+
+        Product product1 = new Product("telefon","elektronika",2000.00,"najlepszy telefon","ekran i reszta bajerów");
+        ShoppingCart cart1 = new ShoppingCart();
+        cart1.addToCart(product1);
+        cart1.addDiscount(20.00);
+        System.out.println(cart1.getTotalPrice());
+        System.out.println(cart1.getTotalPriceAfterDiscount());
+
+        //cart1.toString();
+
     }
+
+
+
 }
