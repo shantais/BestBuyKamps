5889eebf3116b847e19433c26ae5fc31f960636a
Paweł Janowicz <janowiczpawel4@gmail.com>
2023-10-22 17:14:00 +0200

    Adding log in and contact page

--- src/main/java/frontend/js/scripts.js
+++ src/main/java/frontend/js/scripts.js
@@ -1,7 +1,5 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+<script>
+document.getElementById("login").addEventListener("click", function() {
+    window.location.href = "Pages/LOGIN_PAGE.html";});
+</script>
+console.log('Oddzielny plik JavaScript')
