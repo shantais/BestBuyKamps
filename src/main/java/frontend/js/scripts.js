7f3c5c3d38ba617e14c86d6acc0f485c1d3e5c42
Paweł Janowicz <janowiczpawel4@gmail.com>
2023-10-23 12:08:58 +0200

    Adding log in and contact page

--- src/main/java/frontend/js/scripts.js
+++ src/main/java/frontend/js/scripts.js
@@ -3,3 +3,5 @@ document.getElementById("login").addEventListener("click", function() {
     window.location.href = "Pages/LOGIN_PAGE.html";});
 </script>
 console.log('Oddzielny plik JavaScript')
+
+ document.getElementById("cart").addEventListener("click", function() { window.location.href = "CART_PAGE.html";});
