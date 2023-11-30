11f110c97b165de589bc959470fa23ed85b337ea
Paweł Janowicz <janowiczpawel4@gmail.com>
2023-11-30 10:38:19 +0100

    Adding resources direction

--- src/main/resources/static/frontend/js/scripts.js
+++ src/main/resources/static/frontend/js/scripts.js
@@ -1,7 +1,6 @@
-<script>
 document.getElementById("login").addEventListener("click", function() {
     window.location.href = "Pages/LOGIN_PAGE.html";});
-</script>
+
 console.log('Oddzielny plik JavaScript')
 
- document.getElementById("cart").addEventListener("click", function() { window.location.href = "CART_PAGE.html";});
+document.getElementById("cart").addEventListener("click", function() { window.location.href = "CART_PAGE.html";});
